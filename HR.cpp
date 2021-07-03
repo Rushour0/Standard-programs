2cb74f0d8f8e689f3ade968450629c1703351633
Rushikesh Patade <pataderushikesh@gmail.com>
2021-07-03 11:19:23 +0530

    new

---
+++ HR.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+#define loop(i,s,n) for(int i = s;i<n;i++)
+#define ll long long int
+#define f first
+#define s second
+#define mp(a,b) make_pair(a,b);
+#define max_vec(v) *max_element(v.begin(),v.end())
+#define min_vec(v) *min_element(v.begin(),v.end())
+#define vec_in(v,n) do{int temp; for(ll i = 0;i<n;i++){cin>>temp;v.push_back(temp);}}while(0)
+
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main()
+{
+	fastio();
+
+	return 0;
+}
