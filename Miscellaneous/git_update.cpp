0e74e9c34de0c760ab07da9ff63dd941902ab594
Rushikesh Patade <pataderushikesh@gmail.com>
2021-08-04 17:21:41 +0530

    git_updateUPDATED

--- Miscellaneous/git_update.cpp
+++ Miscellaneous/git_update.cpp
@@ -11,11 +11,16 @@ string chartostring(char *a)
 int main(int argc,char **argv)
 {
 	string COMMIT_MESSAGE = "git commit -m \"";
+	if (argc == 1)
+	{
+		cout<< "NO COMMIT MESSAGE";
+		return 1;
+	}
 	for(int i = 1;i<argc;i++) COMMIT_MESSAGE+=argv[i];
 	COMMIT_MESSAGE	+= "\"";
 	system("git add .");
 	system(COMMIT_MESSAGE.c_str());
 	system("git push");
-	
+
 	return 0;
 }
